b16f96cd4c2b218a26a2ed0019574f2353851c62
Андрей <31402327+Andrei281088@users.noreply.github.com>
2019-11-25 14:18:22 +0200

    Repair package identification

--- sandbox/src/main/java/nik/and/MyFirstProgram.java
+++ sandbox/src/main/java/nik/and/MyFirstProgram.java
@@ -3,6 +3,10 @@ package nik.and;
 public class MyFirstProgram {
 
 	public static void main(String[] args){
-		System.out.println("Hello, world!");
+		String text = "World";
+		System.out.println("Hello, " + text + "!");
+		double l = 8.0;
+		double s = l + l;
+		System.out.println("Square of rectangle is - " + s);
 	}
 }
